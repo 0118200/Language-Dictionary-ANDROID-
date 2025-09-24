465de7e20859202b20f9273f4ea59643d125f942
0118200 <donennone@gmail.com>
2025-09-24 14:56:08 +0700

    Update Definition.java

--- Definition.java
+++ Definition.java
@@ -4,7 +4,6 @@ import java.util.List;
 import com.google.gson.annotations.SerializedName;
 
 public class Definition {
-
     @SerializedName("definition")
     private String definition;
 
@@ -17,16 +16,8 @@ public class Definition {
     @SerializedName("antonyms")
     private List<String> antonyms;
 
-    // Getter & Setter
     public String getDefinition() { return definition; }
-    public void setDefinition(String definition) { this.definition = definition; }
-
     public String getExample() { return example; }
-    public void setExample(String example) { this.example = example; }
-
     public List<String> getSynonyms() { return synonyms; }
-    public void setSynonyms(List<String> synonyms) { this.synonyms = synonyms; }
-
     public List<String> getAntonyms() { return antonyms; }
-    public void setAntonyms(List<String> antonyms) { this.antonyms = antonyms; }
 }
