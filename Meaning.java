be13a8cdb948fd6fefe86901e0e8a495bd91f228
0118200 <donennone@gmail.com>
2025-09-24 14:57:38 +0700

    Update Meaning.java

--- Meaning.java
+++ Meaning.java
@@ -2,18 +2,14 @@ package com.example.languagedictionary.model;
 
 import java.util.List;
 import com.google.gson.annotations.SerializedName;
-public class Meaning {
 
+public class Meaning {
     @SerializedName("partOfSpeech")
     private String partOfSpeech;
 
     @SerializedName("definitions")
     private List<Definition> definitions;
 
-    public String getPartOfSpeech() {return partOfSpeech;}
-    public void setPartOfSpeech(String partOfSpeech) {this.partOfSpeech = partOfSpeech;}
-
-    public List<Definition> getDefinitions() {return definitions;}
-    public void setDefinitions(List<Definition> definitions) {this.definitions = definitions;}
-
+    public String getPartOfSpeech() { return partOfSpeech; }
+    public List<Definition> getDefinitions() { return definitions; }
 }
